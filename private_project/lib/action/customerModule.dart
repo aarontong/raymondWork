272f395c7cc86fc5a4b1a3c8f034e142344e1895
Aaron Tong <aarontongwh@gmail.com>
2022-07-15 23:50:53 +0800

    getting the cache back

--- private_project/lib/action/customerModule.dart
+++ private_project/lib/action/customerModule.dart
@@ -15,6 +15,7 @@ import 'package:private_project/widgets/customer/insertField/homeAddressField.da
 import 'package:private_project/widgets/customer/insertField/mobileFIeld.dart';
 import 'package:private_project/widgets/customer/insertField/nameCardImage.dart';
 import 'package:private_project/widgets/customer/insertField/professionFIeld.dart';
+import 'package:private_project/widgets/customer/searchCustomer.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../model/customer.dart';
@@ -23,12 +24,17 @@ class customerModule {
   static late int timestamp1;
   static String customerListJsonString = "";
   late addNewCustomerState state;
+  late searchCustomerState state1;
+
   static final customerModule _customerModule = customerModule._internal();
   factory customerModule(addNewCustomerState state) {
     _customerModule.state = state;
     return _customerModule;
   }
-
+  factory customerModule.searchCustomer(searchCustomerState state) {
+    _customerModule.state1 = state;
+    return _customerModule;
+  }
   customerModule._internal();
 
   Future<void> addNewCustomer(Customer newCustomer, String mobileText) async {
@@ -49,27 +55,42 @@ class customerModule {
     updateCustomerListCache();
   }
 
-  void updateCustomerListCache() {
+  Future<void> updateCustomerListCache() async {
     DatabaseReference ref = FirebaseDatabase.instance.ref("Client");
-    ref.onValue.listen((event) async {
-      final data = event.snapshot.value;
-      String dataString = data.toString();
+    final snapshot = await ref.get();
+    if (snapshot.exists) {
+      customerListJsonString = snapshot.value.toString();
       String fileName = "customerListCache.json";
       var dir = await getTemporaryDirectory();
       File file = File(dir.path + "/" + fileName);
-      file.writeAsStringSync(dataString, flush: true, mode: FileMode.write);
+      file.writeAsStringSync(snapshot.value.toString(),
+          flush: true, mode: FileMode.write);
       DateTime nowDate = DateTime.now();
       timestamp1 = nowDate.millisecondsSinceEpoch;
       final prefs = await SharedPreferences.getInstance();
       await prefs.setInt("customerListCacheTime", timestamp1);
-    });
+      print(snapshot.value);
+    } else {
+      print('No data available.');
+    }
   }
 
-  Future<void> getCachedContent() async {
-    String fileName = "customerListCache.json";
-    var dir = await getTemporaryDirectory();
-    File file = File(dir.path + "/" + fileName);
-    customerListJsonString = await file.readAsString();
+  Future<String> getCachedContent() async {
+    DateTime nowDate = DateTime.now();
+    timestamp1 = nowDate.millisecondsSinceEpoch;
+    final prefs = await SharedPreferences.getInstance();
+    int? cachedTime = prefs.getInt("customerListCacheTime");
+    cachedTime = cachedTime != null ? cachedTime : 0;
+    int difference = timestamp1 - cachedTime;
+    if (difference < 5000 && !customerListJsonString.isEmpty) {
+      String fileName = "customerListCache.json";
+      var dir = await getTemporaryDirectory();
+      File file = File(dir.path + "/" + fileName);
+      customerListJsonString = await file.readAsString();
+    } else {
+      await updateCustomerListCache();
+    }
+    return customerListJsonString;
   }
 
   Future<void> addCustomerProfilePicture(
--- private_project/lib/widgets/customer/searchCustomer.dart
+++ private_project/lib/widgets/customer/searchCustomer.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-
+import 'package:private_project/action/customerModule.dart';
+import 'package:tableview/tableview.dart';
 import 'package:private_project/widgets/customer/addNewCustomer.dart';
 
 class searchCustomerWidget extends StatefulWidget {
@@ -15,14 +16,132 @@ class searchCustomerWidget extends StatefulWidget {
 }
 
 class searchCustomerState extends State<searchCustomerWidget> {
+  static List<List<String>> rowList = [
+    ["阿昌族"],
+    ["白族", "保安族", "布朗族", "布依族"],
+    ["藏族", "朝鲜族"],
+    ["傣族", "达斡尔族", "德昂族", "东乡族", "侗族", "独龙族"],
+    ["鄂伦春族", "俄罗斯族", "鄂温克族"],
+    ["高山族", "仡佬族"],
+    ["哈尼族", "汉族", "哈萨克族", "赫哲族", "回族"],
+    ["景颇族", "京族", "基诺族"],
+    ["柯尔克孜族"],
+    ["拉祜族", "傈僳族", "黎族", "珞巴族"],
+    ["满族", "毛南族", "门巴族", "蒙古族", "苗族", "仫佬族"],
+    ["纳西族", "怒族"],
+    ["普米族"],
+    ["羌族"],
+    ["撒拉族", "畲族", "水族"],
+    ["塔吉克族", "塔塔尔族", "土家族", "土族"],
+    ["佤族", "维吾尔族", "乌孜别克族"],
+    ["锡伯族"],
+    ["瑶族", "彝族", "裕固族"],
+  ];
+  static List<String> headerList = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "P",
+    "Q",
+    "S",
+    "T",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+
   final String title;
   searchCustomerState({Key? key, required this.title});
-
+  var delegate = TableViewDelegate(numberOfSectionsInTableView: () {
+    return 1;
+  }, numberOfRowsInSection: (int section) {
+    return rowList[section].length;
+  }, heightForHeaderInSection: (int section) {
+    return 20;
+  }, heightForRowAtIndexPath: (IndexPath indexPath) {
+    return 40;
+  }, viewForHeaderInSection: (BuildContext context, int section) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.only(left: 10),
+      color: Color.fromRGBO(220, 220, 220, 1),
+      height: 20,
+      child: Text(headerList[section]),
+    );
+  }, cellForRowAtIndexPath: (BuildContext context, IndexPath indexPath) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).pop(rowList[indexPath.section][indexPath.row]);
+      },
+      child: Container(
+        color: Colors.white,
+        height: 40,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 10),
+                child: Text(
+                  rowList[indexPath.section][indexPath.row],
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+            Divider(
+              indent: 10,
+              endIndent: 10,
+              height: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+  });
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    customerModule cm = customerModule.searchCustomer(this);
     return Scaffold(
-      body: Text('hello'),
+      body: FutureBuilder<String>(
+        future: cm.getCachedContent(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (!snapshot.hasData) {
+            // while data is loading:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            // data loaded:
+            final androidDeviceInfo = snapshot.data;
+            return Center(
+              child: TableView(
+                delegate: delegate,
+                scrollbar: TableViewHeaderScrollBar(
+                  headerTitleList: headerList,
+                  itemHeight: 20,
+                  startAlignment: Alignment.centerRight,
+                ),
+              ),
+            );
+          }
+        },
+      ),
       appBar: AppBar(
         title: Text(this.title),
         actions: [
